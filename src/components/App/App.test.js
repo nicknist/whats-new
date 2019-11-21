fd1111809f9b56cc4575fd9284ea56dbf66f7250
Nick Nist <nicknistoc@gmail.com>
2019-11-21 13:54:31 -0700

    Remove data folder

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,10 +4,25 @@ import App from './App';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
-  it('should display the application boilerplate page', () => {
+  it('should display the loading screen page', () => {
     const wrapper = shallow(<App /> );
+    wrapper.setState({ isLoading: true });
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should display an error message when error happens', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ isLoading: false, error: true });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should display main page when not loading and no error', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ error: false, isLoading: false });
+
+    expect(wrapper).toMatchSnapshot();
+  })
 
 })
